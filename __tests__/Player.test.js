4c649f0862cb3aa300864a79063ddf4b8aaf9b4a
marikadicarlo <mdicarlo19@yahoo.com>
2022-01-25 22:48:19 -0500

    test and build the base enemy constructor

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,7 +1,6 @@
 const Potion = require("../lib/Potion");
 
 jest.mock("../lib/Potion");
-console.log(new Potion());
 
 const Player = require("../lib/Player");
 const { expect } = require("@jest/globals");
